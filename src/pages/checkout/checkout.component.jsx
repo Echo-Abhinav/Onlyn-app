3ddabdbe056ffbc97a2eed029507eb119a491e89
Echo-Abhinav <vangaabhinavreddy@gmail.com>
2021-07-29 17:19:04 +0530

    Added stripe.js at checkout page.

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import {
   selectCartItems,
@@ -35,6 +36,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following credit card details for testing the payment*
+      <br />
+      4242 4242 4242 4242 Date: any future date CVV: Any 3-digit number
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
