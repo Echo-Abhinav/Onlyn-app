6aa35712615592e88c37f0a1b889681b954aae1b
Echo-Abhinav <vangaabhinavreddy@gmail.com>
2021-08-18 20:37:48 +0530

    Added compression for gzipping on heroku.

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const compression = require("compression");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-var customer = stripe.customers.create({
-  name: "Jenny Rosen",
-  address: {
-    line1: "510 Townsend St",
-    postal_code: "98140",
-    city: "San Francisco",
-    state: "CA",
-    country: "US",
-  },
-});
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,6 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(compression());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
